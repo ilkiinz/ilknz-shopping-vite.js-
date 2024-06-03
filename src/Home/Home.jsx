b60537b0b0e9865ef5688de4c6984391c30bf43c
ilkiinz <aqayev2205@gmail.com>
2024-06-03 23:07:21 +0400

    first commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,35 @@
 import "./Home.css";
 import Nav from "../Navbar/Nav";
+import Model1 from '../assets/model-girl.jpg';
+import Model2 from "../assets/model-men.jpg";
+import { Link } from 'react-router-dom';
 function Home() {
+    
     return(
         <>
         <Nav/>
-        <div>
-           Home
+        <div className="home-cont">
+                <div className="head-txt-cont">
+
+                    <h1 className="head-txt1">Sustainable. Beautiful. Ethical.</h1>
+                    <Link to={'/shop'}> <button className="btn-shop" >Shop All</button></Link>
+                </div>
+
+                
+                <div className="main-shop-cont">
+                            <div className="main-shop-left">
+                             <img src={Model1} alt="" />
+                             <Link to={'/women'}> <button className="gender-btn">Shop for Women → </button>  </Link>
+                          
+                            </div>
+
+                            <div   className="main-shop-right">
+                               <img src={Model2} alt="" />
+                               <button className="gender-btn">Shop for Men →  </button>
+                            </div>
+                </div>
+
+
         </div>
         </>
     )
--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.png"
 import "./Nav.css";
 function Nav() {
     return (
-        <div>
+        <div className="navbar-container">
             <nav className="navbar-cont">
             <Link to={'/'}><img src={Logo} alt="Logo" className="logo" /></Link>
 
