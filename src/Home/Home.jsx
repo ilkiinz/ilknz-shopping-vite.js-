43101e7d003fb33a1f82340a53ca59ca15e91bed
ilkiinz <aqayev2205@gmail.com>
2024-06-08 00:14:29 +0400

    changed last version

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,9 @@ import "./Home.css";
 import Nav from "../Navbar/Nav";
 import Model1 from '../assets/model-girl.jpg';
 import Model2 from "../assets/model-men.jpg";
+import Model3 from "../assets/card-model1.jpg";
+import Model4 from "../assets/card-model2.jpg";
+import Model5 from "../assets/card-model3.jpg";
 import Example1 from "../assets/show-mag.jpg";
 import Example2 from "../assets/example2.jpg";
 import { Link } from 'react-router-dom';
@@ -32,18 +35,42 @@ function Home() {
                 </div>
 
                 <div className="cards-main-cont">
-                        <img src={Example1} alt="" className="example-1" />
+                    <img src={Example1} alt="" className="example-1" />
                     <div className="info-card">
                         <h1>Ethically Made</h1>
                         <p>
-                        I'm a paragraph. Click here to add your own text and edit me.
-                        I’m a great place for you to tell a story and
-                        let your users know a little more about you.</p>
+                            I'm a paragraph. Click here to add your own text and edit me.
+                            I’m a great place for you to tell a story and
+                            let your users know a little more about you.</p>
                         <Link to={'/about'} className="btn-more"> <button className="btn-more" >Learn More</button></Link>
-                        </div>
-                        <img src={Example2} alt="" className="example-2"/>
-                       
+                    </div>
+                    <img src={Example2} alt="" className="example-2" />
                 </div>
+
+                <div className="cont-models">
+
+                    <h1 className="slide-right">Yeni Sezon Məhsullarına 50% Endirim</h1>
+
+                    <div className="cards-value-cont">
+                        <div className="card-model">
+                            <img src={Model3} alt="" className="img-mod" />
+                            <h1 className="textering">Women</h1>
+                        </div>
+
+                        <div className="card-model">
+                            <img src={Model5} alt="" className="img-mod" />
+                            <h1 className="textering">Couple</h1>
+                        </div>
+
+                        <div className="card-model">
+                            <img src={Model4} alt="" className="img-mod" />
+                            <h1 className="textering">Men</h1>
+                        </div>
+
+                    </div>
+
+                </div>
+
 
             </div>
         </>
