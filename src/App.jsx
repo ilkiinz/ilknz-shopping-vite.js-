d5f14d4c1a332ccb0fcbc926af9cdee6feb94642
ilkiinz <aqayev2205@gmail.com>
2024-06-30 16:41:24 +0400

    changed last version

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import './App.css'
-import { Route, Routes } from 'react-router-dom';
-import Home from "./Home/Home";
-import Women from "./Women/Women"
-import Men from "./Men/Men"
-import Shop from "./Shop All/Shop"
-import About from "./About/About"
-function App() {
- 
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
+const App = () => {
   return (
-    <>
-     <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/women' element={<Women />} />
-        <Route path='/men' element={<Men />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/about' element={<About />} />
-
-       
-      </Routes>
-    </>
+    <div className='app'>
+         <Navbar/>
+    </div>
   )
 }
 
